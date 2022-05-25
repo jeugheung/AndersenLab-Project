ce513f153867b52bdea0ee56dab5f5c24717ceba
Andrey Kim <akart.dev@gmail.com>
2022-05-26 05:19:31 +0600

    remove storyboard

--- Stocks-And/Controllers/StockListViewController.swift
+++ Stocks-And/Controllers/StockListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StockListViewController .swift
 //  Stocks-And
 //
 //  Created by Andrey Kim on 26.05.2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StockListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
-
-
 }
 

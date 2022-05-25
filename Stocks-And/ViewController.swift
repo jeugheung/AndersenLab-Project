214d8dfd9464032c16a962dbc2305e95c31d538b
Andrey Kim <akart.dev@gmail.com>
2022-05-26 04:43:58 +0600

    first commit

---
+++ Stocks-And/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks-And
+//
+//  Created by Andrey Kim on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
